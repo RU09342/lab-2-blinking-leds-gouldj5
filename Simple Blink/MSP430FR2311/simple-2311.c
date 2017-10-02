1be022d84406140108d4f75191d7674d25a9c8c7
Joshua <gouldj5@students.rowan.edu>
2017-10-02 13:06:12 -0400

    multiple 2311 code

--- Simple Blink/MSP430FR2311/simple-2311.c
+++ Simple Blink/MSP430FR2311/simple-2311.c
@@ -9,17 +9,26 @@ int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
 
-	P1DIR |= BIT0;			// configure P1.0 and P1.6 as output
-	P1OUT &= ~BIT0;		// reset the LEDs (so they are off in the start)
+	P1DIR |= (BIT0+BIT1);			// configure P1.0 and P1.6 as output
+	P1OUT &= ~(BIT0+BIT1);		// reset the LEDs (so they are off in the start)
 
 	PM5CTL0 &= ~LOCKLPM5;
 
-	int count = 0;
+	int count = 0;                  //loops 2 LEDs to blink continuously
+	int count1 = 0;
 
-	while (1)
+	while (1)						//while->always
 	{
-		P1OUT ^= 0x01;		// toggle LEDs on and off
-		for
-			(count = 0; count<15000; count++); //from 0-15000 cycles count +1 for delay
+
+		if (count >= 15000) {
+			P1OUT ^= BIT0;
+			count = 0;
+		}
+		if (count1 >= 30000) {
+			P1OUT ^= BIT1;
+			count1 = 0;
+		}
+		count++;
+		count1++;
 	}
 }
