94b926a551c05ab5ed156831cdcea5fcceead565
Joshua <gouldj5@students.rowan.edu>
2017-10-02 10:40:26 -0400

    simple code fix

--- Simple Blink/MSP430G2553/simple-g2553.c
+++ Simple Blink/MSP430G2553/simple-g2553.c
@@ -1,19 +1,19 @@
 #include <msp430.h>
 /*MSP430G2553*/
 /*
-Multiple Blink
+Simple Blink
 50% duty cycle
 */
 
 void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
-	P1DIR |= (BIT0 + BIT6);			// configure P1.0 and P1.6 as output
-	P1OUT &= ~(BIT0 + BIT6);		// reset the LEDs (so they are off in the start)
+	P1DIR |= BIT0;			// configure P1.0 and P1.6 as output
+	P1OUT &= ~BIT0;		// reset the LEDs (so they are off in the start)
 
 	while (1)
 	{
-		P1OUT ^= (BIT0 + BIT6);		// toggle (XOR) P1.0 and P1.6 LEDs
-		__delay_cycles(250000);     // a ~250000uS delay
+		P1OUT ^= 0x01;		// toggle (XOR) P1.0 and P1.6 LEDs
+		for (count=0;count<20000;count++);     // a ~250000uS delay
 	}
 }
